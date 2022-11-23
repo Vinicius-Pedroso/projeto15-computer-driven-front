558adb504d9f4972a479e96bc092f7668bc22f5f
giovanni.padova <gpadova@belmare.com.br>
2022-11-22 22:23:27 -0300

    feat: app.js feito

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import GlobalStyle from "./Assets/GlobalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={}/>
+      <Route path="/cadastro" element={}/>
+      <Route path="/produtos" element={}/>
+      <Route path="/carrinho" element={}/>
+      </Routes>
+      <GlobalStyle />
+    </BrowserRouter>
   );
 }
 
