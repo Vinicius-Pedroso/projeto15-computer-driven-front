a3da694aa6b6760d5ca9ecf4543f6c6faa20fdc2
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-25 15:40:35 -0300

    Feat: Cart setup

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,9 +1,93 @@
+import { useNavigate } from "react-router";
+import { useState } from 'react'
+
 
 export default function cart(){
+
+    const navigate = useNavigate("")
+    const [useProducts, setUseProducts] = useState([])
+
+    function handleOut(){
+
+        setJwt("");
+        navigate("/");
+    }
+
+    function HandleAddProduct (){
+
+    }
+
+    function HandleSubProduct (){
+
+    }
+
+    function HandleRemoveProduct (){
+
+    }
+
+    function DisplayProducts (){
+        
+    }
+
     return (
         <Container>
 
-            
+            <header>
+                <h1>Welcome, {user.name}</h1>
+                <ion-icon name="exit-outline" onClick={handleOut}></ion-icon>
+            </header>
+
+            <div>
+
+                <Products>
+                    <ul>
+                        {products.map((product, id) => (
+                            <li key={id}>
+
+                                <div>
+                                    {/* product data */}
+                                </div>
+
+                                <Button onClick={() => HandleAddProduct()}>
+                                    <ion-icon name="close-circle-outline">Adicionar</ion-icon>
+                                </Button>
+                                <Button onClick={() => HandleSubProduct("entrada")}>
+                                    <ion-icon name="remove-circle-outline">Retirar</ion-icon>
+                                </Button>
+                                <Button onClick={() => HandleRemoveProduct("entrada")}>
+                                    <ion-icon name="close-circle-outline">Deletar</ion-icon>
+                                </Button>
+
+                            </li>
+                        ))}
+                    </ul>
+                </Products>
+
+                <Balance>
+                    <span>Custo total:</span>
+                    <span>R$ {finalPrice}</span>
+                </Balance>
+
+                <section>
+                
+                <Button onClick={() => HandleCheckOut}>Finalizar a compra</Button>
+                </section>
+            </div>
         </Container>
     )
 }
+
+const Container = styled.div`
+    height: 100hv;
+    
+    header {
+        margin-top: 5%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+`
+
+const ProductTemplate = styled.section`
+
+`
--- src/components/checkOut.js
+++ src/components/checkOut.js
@@ -1,4 +1,5 @@
-export default function UserHeader (){
+
+export default function CheckOut (){
     return (
         <>
         </>
