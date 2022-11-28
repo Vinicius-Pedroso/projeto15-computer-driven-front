5984393796f85b29616c2f72f7363dad35b37757
Vinicius <viniciuspedrosoreis@gmail.com>
2022-11-28 12:43:01 -0300

    Fix cart, delete checkout

--- src/Pages/cart.js
+++ src/Pages/cart.js
@@ -2,61 +2,74 @@ import { useNavigate } from "react-router";
 import { useState, useEffect } from 'react'
 import { ProductAddOne, ProductSubOne, ProductRemove } from "../Components/cartFunctions";
 import { RefreshContext } from "../Contexts/RefreshContext";
+import joi from 'joi'
 
-export default function Cart(){
+export default function Cart() {
 
     const { refresh } = useContext(RefreshContext);
     const { jwt, setJwt } = useContext(AuthContext);
     const navigate = useNavigate("")
     const [balance, setBalance] = useState(0)
     const [cartProducts, setCartProducts] = useState([])
+    const [street, setStreet] = useState("")
+    const [city, setCity] = useState("")
+    const [brazillianState, setBrazillianState] = useState("")
+    const [number, setNumber] = useState("")
+    
 
     useEffect(() => {
 
         DisplayProducts();
-    
+
     }, [refresh]);
 
-    function handleOut(){
+    const adressSchema = joi.object({
+        street: joi.string().required().min(3),
+        city: joi.string().required().min(3),
+        brazillianState: joi.string().required().min(3),
+        number: joi.number().required
+    });
+
+    function handleOut() {
 
         setJwt("");
         navigate("/");
     }
 
-    async function HandleAddProduct (productId){
+    async function HandleAddProduct(productId) {
 
         await ProductAddOne(productId, jwt).then((res) => {
             if (res.status === 401) {
-              return console.log("erro ao editar o produto")
+                return console.log("erro ao editar o produto")
             }
         });
 
         DisplayProducts();
     }
 
-    async function HandleSubProduct (productId){
+    async function HandleSubProduct(productId) {
 
         await ProductSubOne(productId, jwt).then((res) => {
             if (res.status === 401) {
-              return console.log("erro ao editar o produto")
+                return console.log("erro ao editar o produto")
             }
         });
-        
+
         DisplayProducts();
     }
 
-    async function HandleRemoveProduct (productId){
+    async function HandleRemoveProduct(productId) {
 
         await ProductRemove(productId, jwt).then((res) => {
             if (res.status === 401) {
-              return console.log("erro ao deletar o produto do carrinho")
+                return console.log("erro ao deletar o produto do carrinho")
             }
         });
 
         DisplayProducts();
     }
 
-    async function DisplayProducts (){
+    async function DisplayProducts() {
 
         let total = 0
 
@@ -71,182 +84,231 @@ export default function Cart(){
         }).catch((err) => {
             return err.response;
         });
-    
+
         setCartProducts(response.data.cartProducts);
 
         response.data.cartProducts.forEach((product) => {
-            total += Number(product.value*product.amount);
-        }) 
-  
+            total += Number(product.value * product.quantity);
+        })
+
         setBalance(total);
     }
 
-    function HandleCheckOut (){
+    function HandleCheckOut(){
 
-        navigate("/pagamento")
+        const userAdress = {
+            street: street,
+            city: city,
+            brazillianState: brazillianState, 
+            number: number
+        }
+
+        const { error } = adressSchema.validate(userAdress, { abortEarly: false });
+
+        if (error) {
+            return alert("Endereço inválido")  
+        }
+
+        reportCompleteCheckout()    
     }
 
     return (
         <Container>
 
             <header>
-                <h1>Welcome, {user.name}</h1>
+                <h2>Bem vindo, {user.name}</h2>
                 <ion-icon name="exit-outline" onClick={handleOut}></ion-icon>
             </header>
 
-            <div>
 
-                <Products>
-                    <ul>
-                        {cartProducts.map((product, id) => (
-                            <li key={id}>
+            <Products>
+                {cartProducts.map((product, id) => (<>
+                    <div className="product">
+                        <img src='product.imgUrl' alt="" />
+                        <div>
+                            <p >{product.name}</p>
+                            <p >{product.quantity} unidade(s)</p>
+                            <p >{product.price}</p>
+                            <p >Total: {product.price * product.quantity}</p>
+                        </div>
+                    </div>
+                    <EditCart>
+                        <div className="edit" onClick={() => HandleAddProduct(product.id)}>
+                            <ion-icon name="close-circle-outline">Adicionar</ion-icon>
+                        </div>
+                        <div className="edit" onClick={() => HandleSubProduct(product.id)}>
+                            <ion-icon name="remove-circle-outline">Retirar</ion-icon>
+                        </div>
+                        <div className="edit" onClick={() => HandleRemoveProduct(product.id)}>
+                            <ion-icon name="close-circle-outline">Deletar</ion-icon>
+                        </div>
+                    </EditCart>
+                    <div className="line"></div>
+                </>))}
+            </Products>
 
-                            <ProdutoCard>
-                                <div className="card">
-                                    <div className="card-img">
-                                    <img src={product.image} alt="" />
-                                    </div>
-                                    <div className="card-info">
-                                    <p className="text-title">{product.name} </p>
-                                    <p className="text-body">{product.description}</p>
-                                    </div>
-                                    <div className="card-footer">
-                                    <span className="text-title">R$ {product.price}</span>
-                                    </div>
-                                </div>
-                            </ProdutoCard>
-                            <EditCart>
-                                <div className="edit" onClick={() => HandleAddProduct(key)}>
-                                    <ion-icon name="close-circle-outline">Adicionar</ion-icon>
-                                </div>
-                                <div className="edit" onClick={() => HandleSubProduct(key)}>
-                                    <ion-icon name="remove-circle-outline">Retirar</ion-icon>
-                                </div>
-                                <div className="edit" onClick={() => HandleRemoveProduct(key)}>
-                                    <ion-icon name="close-circle-outline">Deletar</ion-icon>
-                                </div>
-                            </EditCart>
-                                
-                            </li>
-                        ))}
-                    </ul>
-                </Products>
+            <h3>Custo total: R$ {balance}</h3>
+            <h3>Por favor, insira o endereço da entrega!</h3>
 
-                <Balance>
-                    <span>Custo total:</span>
-                    <span>R$ {balance}</span>
-                </Balance>
+            <div className="inputcolomn">
 
-                <section>
-                
-                <Button onClick={() => HandleCheckOut()}>Finalizar a compra</Button>
-                </section>
+                <input
+                    type="text"
+                    placeholder="Rua"
+                    value={street}
+                    onChange={(e) => setStreet(e.target.value)}
+                />
+                <div className="line"></div>
+                <input
+                    type="text"
+                    placeholder="Cidade"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <div className="line"></div>
+                <input
+                    type="text"
+                    placeholder="Estado (sigla)"
+                    value={brazillianState}
+                    onChange={(e) => setBrazillianState(e.target.value)}
+                />
+                <div className="line"></div>
+                <input
+                    type="text"
+                    placeholder="Número"
+                    value={number}
+                    onChange={(e) => setNumber(e.target.value)}
+                />
             </div>
+            <div className="line"></div>
+
+            <button >Finalizar a compra:</button>
+
+
+            <section>
+
+                <Button onClick={() => HandleCheckOut()}>Finalizar a compra</Button>
+            </section>
         </Container>
     )
 }
 
 const Container = styled.div`
+    padding-top: 5%;
     height: 100hv;
-    
-    header {
-        margin-top: 5%;
+    width: 100%;
+    padding-left: 5%;
+
+    .product{
         display: flex;
-        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+    h2{
+        width: 100%;
+        padding-top: 10px;
+        padding-left: 5%;
+        font-size: 22px;
+        font-family: Poppins;
+        color: black;
+    }
+
+    .product:div {
+        width: 100%;
+        padding-top: 10%;
+        padding-bottom: 10%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    h3{
+        height: 40px;
+        font-size: 22px;
+        display: flex;
+        justify-content: center;
         align-items: center;
     }
-`
 
-const ProdutoCard = styled.div`
-  .card {
-    width: 88vw;
-    height: 254px;
-    border-radius: 10px;
-    padding: 0.8em;
-    font-family: Poppins;
-    background: #f5f5f5;
-    position: relative;
-    overflow: visible;
-    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  }
-  p {
-    font-size: 15px;
-    font-family: Poppins;
-    color: white;
-  }
-
-  .card-img {
-    background-color: #ffcaa6;
-    height: 40%;
-    width: 100%;
-    border-radius: 0.5rem;
-    transition: 0.3s ease;
-  }
-
-  .card-info {
-    padding-top: 5%;
-  }
-
-  svg {
-    width: 20px;
-    height: 20px;
-  }
-
-  .card-footer {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-top: 10px;
-    border-top: 1px solid #ddd;
-  }
-
-  .text-title {
-    font-weight: 900;
-    font-size: 1.2em;
-    line-height: 1.5;
-  }
-
-  .text-body {
-    font-size: 0.9em;
-    padding-bottom: 10px;
-  }
-
-  .card-button {
-    font-size: 80px;
-    display: flex;
-    padding: 0.3em;
-    cursor: pointer;
-
-    transition: 0.3s ease-in-out;
-  }
-
-  .card-img:hover {
-    transform: translateY(-25%);
-    box-shadow: rgba(226, 196, 63, 0.25) 0px 13px 47px -5px,
-      rgba(180, 71, 71, 0.3) 0px 8px 16px -8px;
-  }
-
-  .card-button:hover {
-    border: 1px solid #ffcaa6;
-    background-color: #ffcaa6;
-  }
-`;
-
-const EditCart = styled.div`
-
-    .edit{
-        width: 100px;
-        height: 40px;
-        background-color: grey;
-        border-radius: 5px;
-        display: table-cell;
-        text-align: center;
-        vertical-align: middle;
+    p {
+        width: 100%;
+        padding-top: 10px;
+        padding-left: 5%;
+        font-size: 15px;
+        font-family: Poppins;
+        color: black;
     }
 
+    .inputcolomn {
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        height: 15px;
+        width: 89%;
+        border-radius: 3px;
+        font-size: 15px;
+        font-family: Poppins;
+        color: black;
+        background-color: #f0f0f0;
+        border: 1px none black;
+        ::placeholder {
+            color: black;
+        }
+    }
+
+    img {
+        padding-left: 5%;
+        padding-top: 10px;
+        padding-bottom: 20px;
+        height: 120px;
+        width: 120px;
+        border-radius: 0.5rem;
+    }
+
+    h1{
+        padding-top: 5px;
+        padding-bottom: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 26px;
+        font-family: Poppins;
+        color: #404040;
+    }
+    
+    .line{
+        width: 100vw;
+        height: 2px;
+        background-color: white;
+    }
+
+    button {
+        margin-top: 5%;
+        margin-bottom: 2%;
+        height: 40px;
+        width: 90%;
+        border-radius: 40px;
+        font-size: 15px;
+        font-family: Poppins;
+        color: white;
+        background-color: black;
+        border: 1px none black;
+    }
+
+    .userData{
+        width: 85%;
+        padding-right: 5%;
+        background-color: lightgray;
+        border-radius: 10px;
+        border: none;
+    }
 `
 
-const Balance = styled.div`
+const EditCart = styled.div`
+    padding-bottom: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 
 `
